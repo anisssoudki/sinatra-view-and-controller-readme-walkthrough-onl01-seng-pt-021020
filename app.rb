b94f44145bb6f4b3557f2305793ff4ae63e1006f
Aniss Marrouch <aniss.marrouch@gmail.com>
2020-04-15 05:34:52 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -12,8 +12,13 @@ class App < Sinatra::Base
   end
   
 get '/friends' do
-  @friends = ['Emily Wilding Davison', 'Harriet Tubman', 'Joan of Arc', 'Malala Yousafzai', 'Sojourner Truth']
- 
+  @friends = [
+    'Emily Wilding Davison', 
+    'Harriet Tubman', 
+    'Joan of Arc',
+    'Malala Yousafzai', 
+    'Sojourner Truth'
+    ]
   erb :friends
   end
 end
